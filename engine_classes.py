637cb882525def093998d733d04c8606675c7fa8
vsevo <vdexpert11@yandex.ru>
2022-12-24 20:50:16 +0500

    modifyed engine_classes.py.py

--- engine_classes.py
+++ engine_classes.py
@@ -28,7 +28,7 @@ class Engine(ABC):
 
 class HH(Engine):
     __url = "https://api.hh.ru/vacancies"
-    __per_page = 5
+    __per_page = 100
     __count_geting = 0
 
     def get_request(self, page):
@@ -43,36 +43,36 @@ class HH(Engine):
 
 class SuperJob(Engine):
     __url = f"https://russia.superjob.ru/vacancy/search/?keywords="
+    __per_page = 40
 
-    def get_request(self, page):
+
+    def get_request(self, page, quantity="all_from_page"):
         r = rq.get(f"{self.__url}{self.key_word}&page={page}")
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, "html.parser")
             items = soup.find_all("div", class_="f-test-search-result-item")
             prev_vac = []
-            count_err = 0
 
             for item in items:
                 try:
                     prev_vac.append(
                         {
-                            "name": item.find("span", class_="_2s70W").find("a", class_="_1IHWd").get_text(strip=True),
+                            "name": item.find("span", class_="_2s70W").find("a", class_="_1IHWd").get_text(),
                             "href": "https://russia.superjob.ru" + item.find("a", class_="_1IHWd").get("href"),
-                            "company": item.find("span", class_="_3nMqD").find("a", class_="_1IHWd").get_text(strip=True),
-                            "salary": " ".join(item.find("span", "_2eYAG _1B2ot _3EXZS _3pAka _3GChV").get_text(strip=True).split("\xa0")),
-                            "description": item.find("span", class_="_1G5lt _3EXZS _3pAka _3GChV _2GgYH").get_text(strip=True)
+                            "company": item.find("span", class_="_3nMqD").find("a", class_="_1IHWd").get_text(),
+                            "salary": " ".join(item.find("span", "_2eYAG _1B2ot _3EXZS _3pAka _3GChV").get_text().split("\xa0")),
+                            "description": item.find("span", class_="_1G5lt _3EXZS _3pAka _3GChV _2GgYH").get_text()
                         }
                     )
                 except AttributeError:
-                    count_err += 1
                     continue
 
             dump = [{"items": prev_vac}]
             with open("temp_file.json", "w") as temp_f:
-                json.dump(dump, temp_f, indent=2, separators=(',', ':'))
+                json.dump(dump, temp_f)
 
-            with open("temp_file.json", "r", errors='ignore') as temp_r_f:
-                out_data = json.load(temp_r_f, strict=False)
+            with open("temp_file.json", "r") as temp_r_f:
+                out_data = json.load(temp_r_f)
                 filename = f"SJ-dump-keyword-{self.key_word}"
                 self.get_connector(filename)
                 Connector.all_connectors[filename].insert(dict(*out_data))
